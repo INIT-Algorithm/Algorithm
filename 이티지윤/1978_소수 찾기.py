000f5872ce323194ac3a577c778fa4e0514e9a4e
Jiyun Kim <jiyun4188@gmail.com>
2023-04-13 00:47:46 +0900

    #2 : 1978_소수 찾기

--- 이티지윤/1978_소수 찾기.py
+++ 이티지윤/1978_소수 찾기.py
@@ -5,9 +5,9 @@ nums = list(map(int, input().split())) # 공백으로 숫자 구분
 # 입력받은 n개의 숫자 ex: [1, 3, 5, 7]
 
 prime_cnt=0 #소수 갯수
-isPrime = True #소수 여부
 
 for i in nums:
+    isPrime = True #소수 여부
     if i == 1:
         continue
     for j in range(2, i): # nums에 있는 숫자 i를 2부터 i-1까지 나눔
