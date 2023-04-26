85d858d2f7e8cc6337e28cb23a1df955235eda23
Jiyun Kim <jiyun4188@gmail.com>
2023-04-26 18:17:35 +0900

    #10 : 11279_최대 힙

---
+++ 이티지윤/1966_프린터 큐.py
@@ -0,0 +1,19 @@
+num = int(input())
+
+for i in range(num):
+    N, M = map(int, input().split())
+    queue = list(map(int, input().strip().split()))
+    queue = [(v, idx) for idx, v in enumerate(queue)]
+    
+    order = 0
+    
+    while True:
+        if max(queue)[0] == queue[0][0]:
+            order += 1
+            if queue[0][1] == M:
+                print(order)
+                break
+            else:
+                queue.pop(0)
+        else:
+            queue.append(queue.pop(0))
