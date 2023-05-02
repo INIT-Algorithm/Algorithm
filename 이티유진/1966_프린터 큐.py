49d4e5511baf7a71769fd6ab601e5cf913708a01
yjhss <20211420.sungshin.ac.kr>
2023-05-03 01:15:57 +0900

    #10 : 1966_프린터 큐

--- 이티유진/1966_프린터 큐.py
+++ 이티유진/1966_프린터 큐.py
@@ -1,24 +1,19 @@
 import sys
+from collections import deque
 
-t = int(sys.stdin.readline())                               # 테스트 케이스 개수 t
+t = int(sys.stdin.readline())                           #테스트 케이스 수 t
 
-for i in range (t):
-    n, m = map(int, input().split())
-    queue = list(map(int, input().split()))                 # 리스트 queue
-    cnt = 0                                                 # 문서의 출력 순서를 세기 위한 변수 cnt
-
+for i in range(t):
+    n, m = map(int,input().split())
+    queue = deque(list(map(int,input().split())))       #문서 중요도 queue
+    index_queue = deque(list(range(n)))                 #인덱스 idx_queue
+    cnt = 0                                             #인쇄 순서 cnt
     while queue:
-        if queue[0] == max(queue):                          # 맨 앞 문서의 중요도가 가장 클 때
-            del queue[0]                                    # 맨 앞 문서 삭제
-            m -= 1                                          # 문서 위치 왼쪽으로 한 번 이동하므로 m 값 1 감소
-            cnt +=1                                          # 문서 하나 출력 되었으므로 cnt 값 1 증가
-        else:                                                   # 맨 앞 문서의 중요도가 가장 크지 않을 때
-            queue.append(queue[0])                              # 이 문서를 맨 뒤에 추가  
-            del queue[0]                                        # 맨 앞에서 삭제
-
-            if m == 0:                                          # m이 0일 경우
-                m = len(queue)-1                                # m 맨 뒤 가리킴
-            else:                                               # m이 0이 아닐 경우
-                m -= 1                                          # m 값 1 감소
-    
-    print(cnt)                                                 # cnt 값 출력
+        if queue[0] == max(queue):                      #queue의 첫번째 원소가 최댓값인 경우(가장 앞에 있는 문서의 중요도가 가장 클 경우) pop
+            cnt += 1                                    #pop할 때마다 cnt 1씩 증가
+            queue.popleft()
+            if index_queue.popleft() == m:              #pop한 문서의 인덱스가 m과 같다면 cnt 출력
+                print(cnt)
+        else:
+            queue.append(queue.popleft())               #queue의 첫번째 원소가 최댓값 아닌 경우 맨 뒤로 재배치
+            index_queue.append(index_queue.popleft())
