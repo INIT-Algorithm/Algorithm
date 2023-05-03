69ad8d491ef67aceb55d9d6531d9fd6567e403f7
안예원 <78153914+yewonahn@users.noreply.github.com>
2023-05-04 08:39:13 +0900

    #14 : 11650_좌표 정렬하기
    #14  : Week3_예원이티

---
+++ 이티예원/11650_좌표 정렬하기.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+array = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    array.append((x, y))
+
+array.sort(key = lambda x: (x[0], x[1]))
+
+for x, y in array:
+    print(x, y)
