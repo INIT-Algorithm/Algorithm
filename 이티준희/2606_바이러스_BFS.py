0d64782f7a6dc1b84f3d28a9da5a08510767aef5
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2023-05-18 00:49:31 +0900

    #21 : 2606_바이러스_BFS

--- 이티준희/2606_바이러스_BFS.py
+++ 이티준희/2606_바이러스_BFS.py
@@ -13,6 +13,7 @@
 '''
 
 #bfs 너비 우선 탐색
+'''
 import sys
 from collections import deque
 vertex = int(sys.stdin.readline()) # 컴퓨터 개수
@@ -35,3 +36,39 @@ while queue :
             queue.append(i)
             visited[i]=1
 print(sum(visited)-1)   
+'''
+
+import sys
+from collections import deque
+
+vertex = int(sys.stdin.readline())  # 컴퓨터 개수
+edge = int(sys.stdin.readline())  # 연결선 개수
+
+graph = [[] for i in range(vertex + 1)]  # 그래프
+visited = [0] * (vertex + 1)  # 1번 인덱스부터 사용
+
+for i in range(edge):  # 그래프 생성
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)  # a 에 b 연결
+    graph[b].append(a)  # b 에 a 연결
+
+
+def bfs(start):
+    visited[start] = 1
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        for i in graph[node]:
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = 1
+
+bfs(1)  # 1번 컴퓨터부터 시작
+print(sum(visited) - 1)
+
+
+
+
+
+
