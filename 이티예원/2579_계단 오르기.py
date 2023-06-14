f2297fcade29cd66a79362ac836ec2c1cde9555f
안예원 <78153914+yewonahn@users.noreply.github.com>
2023-06-15 00:00:34 +0900

    #17 : 2579_계단 오르기.py
    #17 : Week4_예원이티

--- 이티예원/2579_계단 오르기.py
+++ 이티예원/2579_계단 오르기.py
@@ -2,16 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-score = []
-dp = []
+s = [0 for i in range(301)]
+dp = [0 for i in range(301)]
 
-for _ in range(n):
-    score.append(int(input()))
+for i in range(n):
+    s[i] = int(input())
 
-dp[0] = score[0]
-dp[1] = score[0] + score[1]
-dp[2] = max(score[0] + score[2], score[1] + score[2])
+dp[0] = s[0]
+dp[1] = s[0] + s[1]
+dp[2] = max(s[1] + s[2], s[0] + s[2])
 for i in range(3, n):
-    dp[i] = max(score[i] + dp[i-2], score[i] + score[i-1] + dp[i-3])
+    dp[i] = max(dp[i-3] + s[i-1] + s[i], dp[i-2] + s[i])
 
 print(dp[n-1])
